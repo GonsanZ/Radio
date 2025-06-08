b3ca61acb47524af42f756ce8066f44291639f58
Gonsanz <lopezgarcia252345@gmail.com>
2025-06-08 08:34:04 -0300

    snapcast.js

--- command/snapcast.js
+++ command/snapcast.js
@@ -1,1 +1,27 @@
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
+const config = require('../config.json');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('snapcast')
+    .setDescription('Elige una estación de Snapcast para reproducir en el canal de voz'),
+
+  async execute(interaction) {
+    const options = Object.entries(config.stations).map(([key, value]) => ({
+      label: value.name,
+      value: key,
+    }));
+
+    const select = new StringSelectMenuBuilder()
+      .setCustomId('select_station')
+      .setPlaceholder('Selecciona una estación')
+      .addOptions(options);
+
+    const row = new ActionRowBuilder().addComponents(select);
+
+    await interaction.reply({
+      content: '🎧 Elige una estación para comenzar a reproducir:',
+      components: [row],
+    });
+  },
+};
